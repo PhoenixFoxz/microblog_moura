25726bf63204ccb88e4ff5c5f738432805766b1d
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-10-10 09:07:16 -0300

    Método inserir

---
+++ src/Usuario.php
@@ -0,0 +1,80 @@
+<?php 
+namespace Microblog;
+Use PDO, Exception;
+class Usuario {
+    private int $id;
+    private string $nome;
+    private string $email;
+    private string $senha;
+    private string $tipo;
+    private PDO $conexao;
+
+    public function __construct()
+    {
+        $this->conexao = Banco::conecta();
+    }
+
+    // Métodos para rotinas de CRUD no Banco
+
+    public function inserir():void {
+        $sql = "INSERT INTO usuario(nome, email, senha, tipo) VALUE(:nome, :email, :senha, :tipo)";
+
+         try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->bindValue(":email", $this->email, PDO::PARAM_STR);
+            $consulta->bindValue(":senha", $this->senha, PDO::PARAM_STR);
+            $consulta->bindValue(":tipo", $this->tipo, PDO::PARAM_STR);
+            $consulta->execute();    
+         } catch (Exception $erro) {
+            die("Erro ao inserir usuário: ".$erro->getMessage());
+         }
+    }
+
+
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function setId(int $id): self {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getSenha(): string {
+        return $this->senha;
+    }
+
+    public function setSenha(string $senha): self {
+        $this->senha = $senha;
+        return $this;
+    }
+
+    public function getTipo(): string {
+        return $this->tipo;
+    }
+
+    public function setTipo(string $tipo): self {
+        $this->tipo = $tipo;
+        return $this;
+    }
+}
