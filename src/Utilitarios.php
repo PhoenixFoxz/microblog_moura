2c8473eb32402051c8d382f0ae9a752fa2621601
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-10-10 11:43:48 -0300

    Criação da classe Utilitarios.php

---
+++ src/Utilitarios.php
@@ -0,0 +1,13 @@
+<?php
+namespace Microblog;
+use PDO, Exception;
+
+abstract class Utilitarios {
+    // Sobre o parâmetro $dados com tipo array/bool
+    // Quando um parâmetro pode receber tipos de dados diferentes de acordo com a chamada do método, usamos o operador || (ou) entre as opções de tipos. Essa sintax é válida a partir do PHP 7.4
+    public static function dump(array | bool $dados):void {
+        echo "<pre>";
+        var_dump($dados);
+        echo "<pre>";
+    }
+}
