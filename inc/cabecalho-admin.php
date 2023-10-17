f0b3aafd06a289d46afa9ce9c035b86b7e514f1b
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-10-17 09:12:57 -0300

    Método excluir

--- inc/cabecalho-admin.php
+++ inc/cabecalho-admin.php
@@ -1,5 +1,11 @@
 <?php
+use Microblog\ControleDeAcesso;
 require_once "../vendor/autoload.php";
+
+// Criando um objeto para acessar os recursos do ControleDeAcesso (ou seja, usando recursos de Sessão do PHP)
+$sessao = new ControleDeAcesso;
+// Executando o método que verifica se tem alguém logado
+$sessao->verificaAcesso();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br" class="h-100">
