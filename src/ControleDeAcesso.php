08cf3976b5d4cde5deaa969673896dda9faaf50c
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-10-17 09:27:25 -0300

    Função Login

--- src/ControleDeAcesso.php
+++ src/ControleDeAcesso.php
@@ -23,4 +23,12 @@ final class ControleDeAcesso {
             die(); // ou exit;
         }
     }
+
+    public function login(int $id, string $nome, string $tipo): void
+    {
+        // No momento em que ocorre o login, criamos variáveis de sessão contendo os dados que queremos monitorar/controlar/usar através da sessão enquanto a pessoa estiver logada.
+        $_SESSION["id"] = $id;
+        $_SESSION["nome"] = $nome;
+        $_SESSION["tipo"] = $tipo;
+    }
 }
