c742e3bf2323a38e8a74adabc13ca0786cf5bd31
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-10-18 11:18:48 -0300

    Criação da classe Categorias e seus métodos

---
+++ src/Categoria.php
@@ -0,0 +1,101 @@
+<?php 
+namespace Microblog;
+use PDO, Exception;
+
+class Categoria {
+    private int $id;
+    private string $nome;
+    private PDO $conexao;
+
+    public function __construct(){
+        $this->conexao = Banco::conecta();
+    }
+
+    public function ler():array{
+        $sql = "SELECT * FROM categorias ORDER BY nome";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->bindValue(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->execute();
+            $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+        } catch (Exception $erro) {
+            die("Erro ao carregar dados: ".$erro->getMessage());
+        }
+
+        return $resultado;
+    }
+
+    public function lerUm():array{
+        $sql = "SELECT * FROM categorias WHERE id = :id";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->execute();
+            $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+        } catch (Exception $erro) {
+            die("Erro ao carregar dados: ".$erro->getMessage());
+        }
+
+        return $resultado;
+    }
+
+    public function inserir():void {
+        $sql = "INSERT INtO categorias(nome) VALUES(:nome)";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao carregar dados: ".$erro->getMessage());
+        }
+
+    }
+
+    public function atualizar():void {
+        $sql = "UPDATE categorias SET nome = :nome WHERE id = :id";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->bindValue(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao carregar dados: ".$erro->getMessage());
+        }
+
+    }
+
+    public function excluir():void {
+        $sql = "DELETE FROM categorias WHERE id = :id";
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao carregar dados: ".$erro->getMessage());
+        }
+
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function setId(int $id): self {
+        $this->id = $id;
+        return $this;
+    }
+}
